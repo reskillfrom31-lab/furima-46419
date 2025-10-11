097e437bb3381cdf0c815c375b19b1baa76ccb93
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-10-11 22:23:44 +0900

    メンター指摘事項任意修正（LGTM取得済）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,8 +10,8 @@ FactoryBot.define do
     #文字数制限はitem_spec.rbに記載
 
     #空欄不可
-    after(:build) do |message|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
     item_name{ Faker::Lorem.characters(number: 40) }
     item_info{ Faker::Lorem.characters(number: 1000) }
