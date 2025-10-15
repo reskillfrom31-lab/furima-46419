760a345eb0ab30f04af061e907e5a30fc6d4a2e0
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-10-15 16:35:42 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action only: [:edit, :update] do
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action only: [:edit, :update, :destroy] do
     redirect_to root_path unless @item.user_id == current_user.id
   end
 
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
