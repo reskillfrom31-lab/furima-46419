369558d2fbbb3b2fa6f08649dd968a02cd33a20e
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-10-29 11:13:44 +0900

    AWS実装

--- Gemfile
+++ Gemfile
@@ -84,3 +84,4 @@ gem 'pry-rails'
 
 gem 'payjp'
 gem 'gon'
+gem "aws-sdk-s3", require: false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-
+require 'aws-sdk-s3'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -34,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  Aws.config.update(
+  ssl_verify_peer: false
+)
 end
