40c897714d423800cbba38e67de0e58c69b62036
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-10-31 20:34:38 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 require_relative "boot"
 
-# **【重要】NoMethodError: private method `warn' called for nil:NilClass 対策**
-# Capistranoデプロイ時の assets:precompile において、SprocketsがRails.loggerの初期化より
-# 早くロードされ、nilに対してメソッドを呼び出してしまう問題を回避します。
-# 修正として、アプリケーションのブート段階よりも更に早くロガーを強制的に設定します。
-unless defined?(Rails.logger) && Rails.logger
+require "rails/all"
+
+# **【重要】Capistrano/Sprockets assets:precompile NoMethodError/NameError 対策**
+# SprocketsがRails.loggerの初期化より早くロードされ、nilに対してメソッドを呼び出す
+# 問題を回避するため、`require "rails/all"`後、Bundlerによるgemロード前に
+# ロガーを強制的に設定します。
+if !Rails.logger
   require 'logger'
   # Rakeタスク実行時に logger が nil の場合に、標準出力を使うロガーを仮設定
-  Rails.logger = Logger.new(STDOUT) 
+  Rails.logger = Logger.new(STDOUT)
   # ログレベルを上げて、assets:precompile実行時の不要な大量出力を抑制します
-  Rails.logger.level = Logger::WARN 
+  Rails.logger.level = Logger::WARN
 end
-
-require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,8 +20,6 @@ Bundler.require(*Rails.groups)
 
 module Furima46419
   class Application < Rails::Application
-    # 以前の config.before_initialize ブロックは削除されました。
-    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
     config.active_storage.variant_processor = :mini_magick
